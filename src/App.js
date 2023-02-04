7d1883ae76c5b9b1b4ebfeb26750a8060fcd312c
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-02-04 11:23:35 +0530

    Expanse Props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import "./styles.css";
 import Expanseitem from "./Components/Expanseitem";
 
-export default function App() {
+function App() {
+  const expenses = [
+    {
+      title: "Food",
+      amount: 200,
+      date: new Date(2022, 7, 14).toString().slice(0, 15),
+      location: "Bihar"
+    },
+    {
+      title: "Bill",
+      amount: 500,
+      date: new Date(2022, 7, 15).toString().slice(0, 15),
+      location: "Jharkhand"
+    },
+    {
+      title: "Roaming",
+      amount: 250,
+      date: new Date(2022, 8, 14).toString().slice(0, 15),
+      location: "Delhi"
+    },
+    {
+      title: "College",
+      amount: 75000,
+      date: new Date(2022, 8, 14).toString().slice(0, 15),
+      location: "Banglore"
+    }
+  ];
   return (
-    <div className="App">
-      <Expanseitem />
-    </div>
+    <>
+      <h2>Let's get started!</h2>
+      <div>
+        <Expanseitem></Expanseitem>
+        {expenses.map((item) => {
+          return (
+            <Expanseitem
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+              location={item.location}
+            ></Expanseitem>
+          );
+        })}
+      </div>
+    </>
   );
 }
+export default App;
