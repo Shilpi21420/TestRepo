367867ce703423d85aa401536faea920a20a55bc
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-02-04 16:25:01 +0530

    Handler

--- Components/Expanseitem.js
+++ Components/Expanseitem.js
@@ -1,18 +1,26 @@
-
 import "./Expenseitems.css";
 import "./ExpanseDate";
+import Card from "./Card";
 import ExpanseDate from "./ExpanseDate";
 
 function Expanseitem(props) {
+  const clickedhandler = () => {
+    console.log("Clicked");
+  };
+  const clickDelete = () => {
+    console.log("Clicked second button");
+  };
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpanseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <h3>{props.location}</h3>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div>
+      <button onClick={clickedhandler}>Change Title</button>
+      <button onClick={clickDelete}>Delete Expense</button>
+    </Card>
   );
 }
 export default Expanseitem;
