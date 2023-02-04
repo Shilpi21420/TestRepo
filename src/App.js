d24ce45b95401e2208cb715b4876cc63bf46bbb2
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-02-04 10:42:39 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Expanseitem from "./Components/Expanseitem";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Expanseitem />
+    </div>
+  );
+}
