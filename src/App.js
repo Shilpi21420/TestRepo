8f65b04874d6990d376ec794498d730479219c7d
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-02-06 16:34:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import Expenses from "./Components/Expenses";
+import React,{ useState } from "react";
+import Expenses from "./Components/NewExpanses/Expenses";
 import NewExpense from "./Components/Expense/NewExpense";
 
+  const dummyexpanses = [
+    {
+      title: "Food",
+      amount: 200,
+      date: new Date(2022, 7, 14).toString().slice(0, 15),
+      location: "Bihar"
+    },
+    {
+      title: "Bill",
+      amount: 500,
+      date: new Date(2022, 7, 15).toString().slice(0, 15),
+      location: "Jharkhand"
+    },
+    {
+      title: "Roaming",
+      amount: 250,
+      date: new Date(2022, 8, 14).toString().slice(0, 15),
+      location: "Delhi"
+    },
+    {
+      title: "College",
+      amount: 75000,
+      date: new Date(2022, 8, 14).toString().slice(0, 15),
+      location: "Banglore"
+    }
+  ];
 function App() {
+
+  const [expenses, setExpenses] = useState(dummyexpanses);
   const addExpanseHandler = expense =>{
-    console.log('In app');
-    console.log(expense);
-  }
+   setExpenses(preExpanses =>{
+    return [expense, ...preExpanses];
+
+   });
+  };
   return (
   <>
-  <NewExpense  onAddExpanse={addExpanseHandler}/>
-  <Expenses />
+  <NewExpense  onAddExpanse= {addExpanseHandler}/>
+  <Expenses items={expenses}/>
   
   </>
   );
