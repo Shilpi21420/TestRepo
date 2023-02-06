f9702e00b884b5be9c277cb215f92a5135899101
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-02-06 11:27:30 +0530

    Update NewExpense.js

--- Components/Expense/NewExpense.js
+++ Components/Expense/NewExpense.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './NewExpense.css';
 import ExpanseForm from './ExpanseForm';
 
-const NewEcpense =() =>{
+const NewEcpense =(props) =>{
+    const saveExpenseDataandler = (enterExpenseData) =>{
+        const expenseDate = {
+            ...enterExpenseData,
+            id: Math.random().toString()
+        };
+       props.onAddExpanse(expenseDate);
+
+    };
 return (
 <div className='new-expense'>
-    <ExpanseForm />
+    <ExpanseForm  onSaveExpenseData={saveExpenseDataandler} />
 </div>
 
     
