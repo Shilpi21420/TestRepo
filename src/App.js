2ede31e67ebe3189f0bd1da37d9063a6eb1c1043
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-02-06 11:28:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,59 +1,17 @@
-// import "./styles.css";
-// import Expanseitem from "./Components/Expanseitem";
-
-// function App() {
-//   const expenses = [
-//     {
-//       title: "Food",
-//       amount: 200,
-//       date: new Date(2022, 7, 14).toString().slice(0, 15),
-//       location: "Bihar"
-//     },
-//     {
-//       title: "Bill",
-//       amount: 500,
-//       date: new Date(2022, 7, 15).toString().slice(0, 15),
-//       location: "Jharkhand"
-//     },
-//     {
-//       title: "Roaming",
-//       amount: 250,
-//       date: new Date(2022, 8, 14).toString().slice(0, 15),
-//       location: "Delhi"
-//     },
-//     {
-//       title: "College",
-//       amount: 75000,
-//       date: new Date(2022, 8, 14).toString().slice(0, 15),
-//       location: "Banglore"
-//     }
-//   ];
-//   return (
-//     <>
-//       <h2>Let's get started!</h2>
-//       <div>
-//         <Expanseitem></Expanseitem>
-//         {expenses.map((item) => {
-//           return (
-//             <Expanseitem
-//               title={item.title}
-//               amount={item.amount}
-//               date={item.date}
-//               location={item.location}
-//             ></Expanseitem>
-//           );
-//         })}
-//       </div>
-//     </>
-//   );
-// }
-// export default App;
-
-
 import Expenses from "./Components/Expenses";
-import "./styles.css";
+import NewExpense from "./Components/Expense/NewExpense";
 
 function App() {
-  return <Expenses />;
+  const addExpanseHandler = expense =>{
+    console.log('In app');
+    console.log(expense);
+  }
+  return (
+  <>
+  <NewExpense  onAddExpanse={addExpanseHandler}/>
+  <Expenses />
+  
+  </>
+  );
 }
 export default App;
