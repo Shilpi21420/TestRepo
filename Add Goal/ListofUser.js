7ea02f6450e621a7b8f26914c642c899d00faa03
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-02-07 17:36:45 +0530

    Add files via upload

---
+++ Add Goal/ListofUser.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from './ListofUse.module.css';
+
+const ListofUser = (props) =>{
+    return (
+      <Card className={classes.users}>
+        <ul>
+          {props.users.map((user) => (
+            <li key={user.id}> 
+                {user.name} ({user.age} Years old) {user.collage}
+            </li>
+           
+          ))}
+        </ul>
+        </Card>
+    );
+
+}
+export default ListofUser;
