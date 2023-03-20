8d5974d2003a9f48bc1c4fbeade49938d54497d6
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-03-20 11:42:16 +0530

    Add files via upload

---
+++ Movie/components/Form.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Form = ()=>{
+    return(
+        <form>
+              <label> Title<input type="text" name="name" /></label>
+              <label> Opening Text<input type="text" name="name" /></label>
+              <label> Realease Date<input type="text" name="name" /></label>
+              <button >Add Movie</button>
+            
+        </form>
+
+    );
+
+
+}
+export default Form;
